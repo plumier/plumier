56f2ec994e53086de9b3bccac9dbf581d6dab854
Ketut Sandiarsa <ktutnik@gmail.com>
2018-07-20 13:53:13 +0800

    Reinclude my-module.js

---
+++ packages/core/test/resolve-path/my-module.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class MyModule {
+}
+exports.MyModule = MyModule;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibXktbW9kdWxlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsibXktbW9kdWxlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUE7Q0FBd0I7QUFBeEIsNEJBQXdCIn0=
