f5669b39c34dee91fae9a61f54d884ddd2c62054
Ketut Sandiarsa <ktutnik@gmail.com>
2019-09-17 11:42:36 +0800

    Add support for array value on ActionResult.setHeader

--- packages/plumier/test/integration/application/action-result.spec.ts
+++ packages/plumier/test/integration/application/action-result.spec.ts
@@ -1,6 +1,5 @@
-import Plumier, { response, route, RestfulApiFacility } from "plumier"
+import Plumier, { response, route, RestfulApiFacility, ActionResult } from "plumier"
 import supertest from "supertest"
-
 
 describe("Redirect Action Result", () => {
     it("Should be able to redirect result", async () => {
@@ -25,5 +24,25 @@ describe("Redirect Action Result", () => {
             .expect((resp:supertest.Response) => {
                 expect(resp.header.location).toBe("/animal/hello")
             })
+    })
+
+    it("Should be able to set array of string on setHeader", async () => {
+        class AnimalController {
+            @route.get()
+            index(){
+                return new ActionResult({hello: "World"})
+                    .setHeader("Set-Cookie", ["value1", "value2"])
+            }
+        }
+
+        const plumier = await new Plumier()
+            .set(new RestfulApiFacility())
+            .set({ controller: AnimalController })
+            .set({ mode: "production" })
+            .initialize()
+        const response = await supertest(plumier.callback())
+            .get("/animal/index")
+            .expect(200)
+        expect(response.header["set-cookie"]).toMatchObject(["value1", "value2"])
     })
 })
