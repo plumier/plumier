12c5c629f2ce13c3be8dcae803739e9a5d82bd3d
Ketut Sandiarsa <ktutnik@gmail.com>
2019-11-01 18:05:27 +0800

    chore: Upgrade dependencies: Koa, Validator, TypedConverter

--- update.sh
+++ update.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-npx npm-check-updates
-npx npm-check-updates -a --packageFile packages/core/package.json
-npx npm-check-updates -a --packageFile packages/jwt/package.json
-npx npm-check-updates -a --packageFile packages/mongoose/package.json
-npx npm-check-updates -a --packageFile packages/multipart/package.json
-npx npm-check-updates -a --packageFile packages/plumier/package.json
-npx npm-check-updates -a --packageFile packages/serve-static/package.json
-npx npm-check-updates -a --packageFile packages/social-login/package.json
+ncu -a
+ncu -a --packageFile packages/core/package.json
+ncu -a --packageFile packages/jwt/package.json
+ncu -a --packageFile packages/mongoose/package.json
+ncu -a --packageFile packages/multipart/package.json
+ncu -a --packageFile packages/plumier/package.json
+ncu -a --packageFile packages/serve-static/package.json
+ncu -a --packageFile packages/social-login/package.json
+rm -f yarn.lock 
+rm -rf node_modules
 yarn install
 yarn test
