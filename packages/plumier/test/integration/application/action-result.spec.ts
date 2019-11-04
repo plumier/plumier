fdf6d25f535d150f13a6e44960debe31e4305d9b
Ketut Sandiarsa <ktutnik@gmail.com>
2019-11-04 13:40:52 +0800

    fix: Expose setCookie method on ActionResult (#238)

--- packages/plumier/test/integration/application/action-result.spec.ts
+++ packages/plumier/test/integration/application/action-result.spec.ts
@@ -22,7 +22,7 @@ describe("Redirect Action Result", () => {
         await supertest(plumier.callback())
             .get("/animal/index")
             .expect(302)
-            .expect((resp:supertest.Response) => {
+            .expect((resp: supertest.Response) => {
                 expect(resp.header.location).toBe("/animal/hello")
             })
     })
@@ -32,8 +32,8 @@ describe("Redirect Action Result", () => {
             @route.get()
             index() {
                 return response
-                .json({foo: "bar"})
-                .setHeader('set-cookie',["foo=bar;","message=hello;"])
+                    .json({ foo: "bar" })
+                    .setHeader('set-cookie', ["foo=bar;", "message=hello;"])
             }
         }
         const plumier = await new Plumier()
@@ -44,12 +44,33 @@ describe("Redirect Action Result", () => {
         await supertest(plumier.callback())
             .get("/animal/index")
             .expect(200)
-            .expect((resp:supertest.Response) => {
+            .expect((resp: supertest.Response) => {
                 const cookie = resp.header["set-cookie"]
                 if (process.versions.node.substr(0, 1) === "8") //check if current node.js is version 8
                     expect(cookie).toMatchObject(["foo=bar;,message=hello;"])
                 else
                     expect(cookie).toMatchObject(["foo=bar;", "message=hello;"])
+            })
+    })
+
+    it("Should be able to set cookie", async () => {
+        class AnimalController {
+            @route.get()
+            index() {
+                return response.json({ lorem: 1 })
+                    .setCookie("theName", "lorem ipsum")
+            }
+        }
+        const plumier = await new Plumier()
+            .set(new RestfulApiFacility())
+            .set({ controller: AnimalController })
+            .set({ mode: "production" })
+            .initialize()
+        await supertest(plumier.callback())
+            .get("/animal/index")
+            .expect(200)
+            .expect((resp: supertest.Response) => {
+                expect(resp.get("Set-Cookie")[0]).toBe("theName=lorem ipsum; path=/; httponly")
             })
     })
 })
