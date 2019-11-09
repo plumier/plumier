3abae866e757be470977f4f18e90a97c97d2ec9e
Ketut Sandiarsa <ktutnik@gmail.com>
2019-11-09 23:10:02 +0800

    fix: Expose route analyzer data types

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,7 +6,7 @@ export { pipe } from "./middleware-pipeline";
 export { response } from "./response";
 export { analyzeRoutes, generateRoutes, printAnalysis } from "./route-generator";
 export { router } from "./router";
-export {  ValidationMiddleware } from "./validator";
+export { ValidationMiddleware } from "./validator";
 export { Class, consoleLog, findFilesRecursive, getChildValue, hasKeyOf, isCustomClass } from "./common";
 export { AuthDecoratorImpl, authorize } from "./decorator.authorize";
 export { bind } from "./decorator.bind";
@@ -17,6 +17,6 @@ export {
     ActionResult, Application, AuthorizeMetadataInfo, AuthorizeStore, Configuration, DefaultFacility,
     DependencyResolver, Facility, FileParser, FileUploadInfo, HttpMethod, HttpStatusError, Invocation, KoaMiddleware,
     Middleware, MiddlewareDecorator, MiddlewareUtil, PlumierApplication, PlumierConfiguration, RedirectActionResult,
-    RouteContext, RouteInfo, ValidatorDecorator, ValidatorFunction, ValidatorInfo, ValidatorStore, ValidationError, 
-    errorMessage
+    RouteContext, RouteInfo, RouteAnalyzerFunction, RouteAnalyzerIssue, ValidatorDecorator, ValidatorFunction,
+    ValidatorInfo, ValidatorStore, ValidationError, errorMessage
 } from "./types";
