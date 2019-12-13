2c44ddd750d88e306eba17e5fa530b39b6879fee
Ketut Sandiarsa <ktutnik@gmail.com>
2019-12-14 06:11:11 +0800

    refactor: Refactor router (#289)
    * feat: Rest decorator for restful API conventient
    
    * refactor: Refactor router

--- packages/core/src/router.ts
+++ packages/core/src/router.ts
@@ -20,52 +20,51 @@ interface RouteMatcher {
 // ------------------------------- HELPER ------------------------------ //
 // --------------------------------------------------------------------- //
 
-function toRegExp(route: RouteInfo, path: string): RouteMatcher {
-    const keys: Key[] = []
-    const regexp = pathToRegexp(route.url, keys)
-    const match = regexp.exec(path)
-    const query = !match ? {} : keys.reduce((a, b, i) => {
-        a[b.name] = match![i + 1]
-        return a;
-    }, <any>{})
-    return { match, query, method: route.method.toUpperCase(), route }
-}
-
-function getMatcher(infos: RouteInfo[], ctx: Context) {
-    return infos.map(x => toRegExp(x, ctx.path)).find(x => Boolean(x.match) && x.method == ctx.method)
-}
-
 function sendError(ctx: Context, status: number, message: any) {
     ctx.status = status
     ctx.body = { status, message }
 }
 
+function getHandler(ctx: Context) {
+    for (const route of ctx.routes) {
+        if (route.method !== ctx.method.toLowerCase()) continue
+        const keys: Key[] = []
+        const regexp = pathToRegexp(route.url, keys)
+        const match = regexp.exec(ctx.path)
+        if (match) {
+            const query = keys.reduce((a, b, i) => {
+                a[b.name] = match![i + 1]
+                return a;
+            }, ctx.request.query)
+            return { route, query }
+        }
+    }
+}
+
+
 /* ------------------------------------------------------------------------------- */
 /* ------------------------------- ROUTER ---------------------------------------- */
 /* ------------------------------------------------------------------------------- */
 
-function router(infos: RouteInfo[], config:Configuration) {
+function router(infos: RouteInfo[], config: Configuration) {
     const matchCache = new Map<string, RouteMatcher | undefined>()
-    const getMatcherCached = useCache(matchCache, getMatcher, (info, ctx) => `${ctx.method}${ctx.path}`)
+    const getHandlerCached = useCache(matchCache, getHandler, (ctx) => `${ctx.method}${ctx.path}`)
     return async (ctx: Context) => {
         try {
             ctx.config = config
             ctx.routes = infos
-            const match = getMatcherCached(infos, ctx)
-            if (match) {
-                for (const key in match.query) {
-                    const element = match.query[key];
-                    ctx.request.query[key] = element
-                }
-                ctx.route = match.route
+            const handler = getHandlerCached(ctx)
+            if (handler) {
+                ctx.request.query = handler.query
+                ctx.route = handler.route
             }
             const result = await pipe(ctx)
             await result.execute(ctx)
         }
         catch (e) {
-            if (e instanceof ValidationError) 
+            if (e instanceof ValidationError)
                 sendError(ctx, e.status, e.issues)
-            else if (e instanceof HttpStatusError) 
+            else if (e instanceof HttpStatusError)
                 sendError(ctx, e.status, e.message)
             else
                 ctx.throw(e)
