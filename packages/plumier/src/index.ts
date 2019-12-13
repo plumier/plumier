79ef47ef555a8f22e51daaa0563ecba4b6891466
Ketut Sandiarsa <ktutnik@gmail.com>
2019-12-13 19:18:50 +0800

    feat: Rest decorator for restful API conventient (#288)

--- packages/plumier/src/index.ts
+++ packages/plumier/src/index.ts
@@ -1,13 +1,14 @@
-export { 
-    ActionContext, ActionResult, Application, AsyncValidatorResult, 
-    AuthorizationContext, authorize, Authorizer, bind, binder, Class, 
-    Configuration, CustomValidator, CustomValidatorFunction, 
-    DefaultDependencyResolver, DefaultFacility, DependencyResolver, 
-    domain, Facility, FileParser, FileUploadInfo, HeaderPart, 
-    HttpMethod, HttpStatus, HttpStatusError, Invocation, KoaMiddleware, 
-    middleware, Middleware, MiddlewareUtil, ParameterBinderMiddleware, 
-    PlumierApplication, PlumierConfiguration, RequestPart, response, route, RouteInfo, 
-    val, validate, ValidatorContext, ValidatorMiddleware 
+export {
+    ActionContext, ActionResult, Application, AsyncValidatorResult,
+    AuthorizationContext, authorize, Authorizer, bind, binder, Class,
+    Configuration, CustomValidator, CustomValidatorFunction,
+    DefaultDependencyResolver, DefaultFacility, DependencyResolver,
+    domain, Facility, FileParser, FileUploadInfo, HeaderPart,
+    HttpMethod, HttpStatus, HttpStatusError, Invocation, KoaMiddleware,
+    middleware, Middleware, MiddlewareUtil, ParameterBinderMiddleware,
+    PlumierApplication, PlumierConfiguration, RequestPart, response, route, RouteInfo,
+    val, validate, ValidatorContext, ValidatorMiddleware, rest, RestDecoratorImpl,
+    RouteDecoratorImpl
 } from "@plumier/core"
 export * from "./facility"
 
