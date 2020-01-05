00f38d40feea5ec0680e896b6d8bb29fb0d70546
Ketut Sandiarsa <ktutnik@gmail.com>
2020-01-05 10:43:22 +0800

    refactor: Refactor validator to make it more readable

--- packages/core/src/validator.ts
+++ packages/core/src/validator.ts
@@ -14,7 +14,6 @@ import {
     ValidatorContext,
     ValidatorDecorator,
 } from "./types"
-import { route } from './decorator.route'
 
 
 // --------------------------------------------------------------------- //
@@ -22,26 +21,16 @@ import { route } from './decorator.route'
 // --------------------------------------------------------------------- //
 
 
-interface AsyncValidatorItem {
+interface CustomValidatorNode {
     value: any,
     path: string,
     parent?: { value: any, type: Class, decorators: any[] }
     validator: CustomValidatorFunction | string | symbol | CustomValidator
 }
 
-function createVisitor(items: AsyncValidatorItem[]) {
-    return (i: tc.VisitorInvocation) => {
-        const result = i.proceed();
-        const decorators: ValidatorDecorator[] = i.decorators.filter((x: ValidatorDecorator) => x.type === "ValidatorDecorator")
-        if (isCustomClass(i.type)) {
-            const meta = reflect(i.type)
-            const classDecorators = meta.decorators.filter((x: ValidatorDecorator) => x.type === "ValidatorDecorator")
-            decorators.push(...classDecorators)
-        }
-        if (decorators.length > 0)
-            items.push(...decorators.map(x => ({ value: i.value, path: i.path, parent: i.parent, validator: x.validator })))
-        return result;
-    }
+interface ValidationResult {
+    result: any[],
+    issues: tc.ResultMessages[]
 }
 
 tc.val.custom = (val: CustomValidatorFunction | string | symbol | CustomValidator) => {
@@ -56,9 +45,33 @@ tc.val.result = (a: string, b: string | string[]) => {
 // --------------------------------------------------------------------- //
 // ------------------------------- HELPER ------------------------------ //
 // --------------------------------------------------------------------- //
+
+// Plumier supported custom async validation to possibly execute long running task inside custom validation. 
+// Since typedconverter doesn't support async validation, thus it should executed separately (sync and async)
+// Validation process is a traversal process through deep data type properties that make separate execution inefficient. 
+// To solve the issue, a collection of custom validation decorator created during the sync process 
+// using typed converter VisitorExtension then saved into collection of CustomValidatorNode.
+// Furthermore async validation process doesn't need to do the traversal process because all the required data already provided
+
 const getName = (path: string) => path.indexOf(".") > -1 ? path.substring(path.lastIndexOf(".") + 1) : path
 
-async function validateAsync(x: AsyncValidatorItem, ctx: ActionContext): Promise<AsyncValidatorResult[]> {
+//custom visitor extension to gather the CustomValidatorNode
+function customValidatorNodeVisitor(items: CustomValidatorNode[]) {
+    return (i: tc.VisitorInvocation) => {
+        const result = i.proceed();
+        const decorators: ValidatorDecorator[] = i.decorators.filter((x: ValidatorDecorator) => x.type === "ValidatorDecorator")
+        if (isCustomClass(i.type)) {
+            const meta = reflect(i.type)
+            const classDecorators = meta.decorators.filter((x: ValidatorDecorator) => x.type === "ValidatorDecorator")
+            decorators.push(...classDecorators)
+        }
+        if (decorators.length > 0)
+            items.push(...decorators.map(x => ({ value: i.value, path: i.path, parent: i.parent, validator: x.validator })))
+        return result;
+    }
+}
+
+async function validateNode(x: CustomValidatorNode, ctx: ActionContext): Promise<AsyncValidatorResult[]> {
     const name = getName(x.path)
     const info: ValidatorContext = { ctx, name, parent: x.parent }
     if (x.value === undefined || x.value === null) return []
@@ -79,36 +92,62 @@ async function validateAsync(x: AsyncValidatorItem, ctx: ActionContext): Promise
     }))
 }
 
-async function validate(ctx: ActionContext) {
-    if(ctx.route.action.parameters.length === 0) return []
-    const decsAsync: AsyncValidatorItem[] = []
-    const visitors = [createVisitor(decsAsync), ...(ctx.config.typeConverterVisitors || [])]
-    const result: any[] = []
+
+// --------------------------------------------------------------------- //
+// -------------------------- IMPLEMENTATIONS -------------------------- //
+// --------------------------------------------------------------------- //
+
+function validateSync(ctx: ActionContext, visitors: tc.VisitorExtension[]): ValidationResult {
+    const result = []
     const issues: tc.ResultMessages[] = []
-    //sync validations
     for (const [index, parMeta] of ctx.route.action.parameters.entries()) {
         const rawParameter = ctx.parameters[index]
         const parValue = tc.validate(rawParameter, {
             decorators: parMeta.decorators, path: parMeta.name, type: parMeta.type || Object,
-            visitors: visitors, guessArrayElement: !!ctx.is("urlencoded")
+            visitors, guessArrayElement: !!ctx.is("urlencoded")
         })
         result.push(parValue.value)
         if (parValue.issues)
             issues.push(...parValue.issues)
     }
-    //async validations
-    if (decsAsync.length > 0) {
-        const results = await Promise.all(decsAsync.map(async x => validateAsync(x, ctx)))
-        for (const invalid of results.flatten()) {
-            const msg = issues.find(x => x.path === invalid.path)
-            if (msg) msg.messages.push(...invalid.messages)
-            else issues.push(invalid)
+    return { result, issues }
+}
+
+async function validateAsync(ctx: ActionContext, nodes: CustomValidatorNode[]): Promise<ValidationResult> {
+    if (nodes.length === 0) return { result: [], issues: [] }
+    const results = await Promise.all(nodes.map(async x => validateNode(x, ctx)))
+    return { result: [], issues: results.flatten() }
+}
+
+
+function mergeMessage(messages: tc.ResultMessages[]){
+    const hash:{[key:string]: tc.ResultMessages} = {}
+    const result:tc.ResultMessages[] = []
+    for (const invalid of messages) {
+        if(!!hash[invalid.path]){
+            hash[invalid.path].messages.push(...invalid.messages)
+        }
+        else {
+            hash[invalid.path] = invalid
+            result.push(invalid)
         }
     }
-    if (issues.length > 0)
-        throw new ValidationError(issues.map(x => ({ path: x.path.split("."), messages: x.messages })));
-    return result
+    return result;
 }
+
+async function validate(ctx: ActionContext) {
+    if (ctx.route.action.parameters.length === 0) return []
+    const nodes: CustomValidatorNode[] = []
+    const visitors = [ customValidatorNodeVisitor(nodes), ...(ctx.config.typeConverterVisitors || [])]
+    const syncResult = validateSync(ctx, visitors)
+    const asyncResult = await validateAsync(ctx, nodes)
+    const issues = syncResult.issues.concat(asyncResult.issues)
+    if (issues.length > 0){
+        throw new ValidationError(mergeMessage(issues).map(x => ({ path: x.path.split("."), messages: x.messages })));
+    }
+    return syncResult.result
+}
+
 
 class ValidatorMiddleware implements Middleware {
     async execute(invocation: Readonly<Invocation<ActionContext>>): Promise<ActionResult> {
