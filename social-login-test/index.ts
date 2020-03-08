4f399c0161f68b4111ea8e3d9c5b3ce9b0af13d6
Ketut Sandiarsa <ktutnik@gmail.com>
2020-03-08 15:40:51 +0800

    test: Add social login manual test (#383)

---
+++ social-login-test/index.ts
@@ -0,0 +1,38 @@
+import { ServeStaticFacility } from "@plumier/serve-static"
+import {
+    FacebookOAuthFacility,
+    GitHubOAuthFacility,
+    GitLabOAuthFacility,
+    GoogleOAuthFacility,
+    OAuthUser,
+    redirectUri,
+    TwitterOAuthFacility,
+} from "@plumier/social-login"
+import dotenv from "dotenv"
+import { join } from "path"
+import Plumier, { bind, response, route, WebApiFacility } from "plumier"
+
+dotenv.config()
+
+export class AuthController {
+    @route.get("/")
+    index() {
+        return response.file(join(__dirname, "./www/index.html"))
+    }
+
+    @redirectUri()
+    callback(@bind.oAuthUser() user: OAuthUser) {
+        return response.postMessage({ status: "Success", user })
+    }
+}
+
+new Plumier()
+    .set(new WebApiFacility({ controller: __filename, bodyParser: { multipart: true } }))
+    .set(new ServeStaticFacility())
+    .set(new FacebookOAuthFacility())
+    .set(new GoogleOAuthFacility())
+    .set(new GitHubOAuthFacility())
+    .set(new GitLabOAuthFacility())
+    .set(new TwitterOAuthFacility())
+    .listen(3000)
+    .catch(console.error)
---
+++ social-login-test/www/script.js
@@ -0,0 +1,36 @@
+// function to open a dialog window at the center of the screen
+function modal(url) {
+    const h = 400; const w = 500;
+    const y = window.top.outerHeight / 2 + window.top.screenY - (h / 2);
+    const x = window.top.outerWidth / 2 + window.top.screenX - (w / 2);
+    const option = [
+        ["toolbar", "no"], ["location", "no"], ["directories", "no"], ["status", "no"],
+        ["menubar", "no"], ["scrollbars", "no"], ["resizable", "no"], ["copyhistory", "no"],
+        ["width", w], ["height", h], ["top", y], ["left", x]
+    ]
+    window.open(url, undefined, option.map(([a, b]) => `${a}=${b}`).join(", "));
+}
+
+// render user profile
+function render(user) {
+    document.querySelector(".profile").innerHTML = `
+        <img src="${user.profilePicture}"/>
+        <div>
+            <h2>${user.name}</h2>
+            <span>${user.provider}</span>
+        </div>
+    `;
+}
+
+function init() {
+    // load the csrf secret for social login dialog
+    fetch("/auth/csrf-secret")
+
+    // message sent by server response.postMessage() received here
+    window.addEventListener("message", evt => {
+        if (evt.origin === location.origin && evt.data.status === "Success") {
+            evt.source.close();
+            render(evt.data.user)
+        }
+    });
+}
