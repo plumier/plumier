82a57bd113b73341f38cafb5fe702e5414f47f13
Ketut Sandiarsa <ktutnik@gmail.com>
2020-06-10 06:20:40 +0800

    refactor: Simplified  Open API component generation logic (#489)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -10,7 +10,7 @@ export { analyzeRoutes, printAnalysis } from "./route-analyzer";
 export { router } from "./router";
 export { Class, consoleLog, findFilesRecursive, getChildValue, hasKeyOf, isCustomClass, printTable, toBoolean, cleanupConsole, ellipsis, analyzeModel, AnalysisMessage } from "./common";
 export { AuthDecoratorImpl, authorize } from "./decorator/authorize";
-export { ApiDescriptionDecorator, ApiEnumDecorator, ApiFieldNameDecorator, ApiRequiredDecorator, ApiResponseDecorator, ApiTagDecorator, api } from "./decorator/api"
+export { ApiDescriptionDecorator, ApiEnumDecorator, ApiFieldNameDecorator, ApiRequiredDecorator, ApiResponseDecorator, ApiTagDecorator, api, ApiHidePropertyDecorator } from "./decorator/api"
 export { bind } from "./decorator/bind";
 export { domain, middleware } from "./decorator/common";
 export { route, RouteDecoratorImpl } from "./decorator/route";
