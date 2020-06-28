eba5ecf93fff33f0d433217cc642b190d8447851
Ketut Sandiarsa <ktutnik@gmail.com>
2020-06-28 09:14:16 +0800

    fix: Fix mongoose repository isolation mode

--- packages/mongoose/src/index.ts
+++ packages/mongoose/src/index.ts
@@ -1,7 +1,7 @@
 import "./validator"
 
 import { model } from "./generator"
-export { model, MongooseHelper, getDefinition, getModels, models } from "./generator"
+export { model, MongooseHelper, getDefinition, getModels, models, globalHelper } from "./generator"
 export { collection } from "./decorator"
 export { MongooseFacility, MongooseGenericControllerFacility } from "./facility"
 export { MongooseControllerGeneric, MongooseOneToManyControllerGeneric, MongooseRepository, MongooseOneToManyRepository } from "./generic-controller"
