f750d629bdb17e0a72d5be7bb1f9c770c2f07c62
Ketut Sandiarsa <ktutnik@gmail.com>
2020-06-30 13:33:15 +0800

    fix: Fix add optional parameter to custom repository on generic controller

--- packages/typeorm/src/generic-controller.ts
+++ packages/typeorm/src/generic-controller.ts
@@ -1,7 +1,8 @@
-import { RepoBaseControllerGeneric, RepoBaseOneToManyControllerGeneric } from "@plumier/generic-controller"
+import { RepoBaseControllerGeneric, RepoBaseOneToManyControllerGeneric, OneToManyRepository, Repository } from "@plumier/generic-controller"
 import reflect, { generic } from "tinspector"
 
 import { TypeORMOneToManyRepository, TypeORMRepository } from "./repository"
+import { Class } from '@plumier/core'
 
 // --------------------------------------------------------------------- //
 // ------------------------ GENERIC CONTROLLERS ------------------------ //
@@ -10,16 +11,16 @@ import { TypeORMOneToManyRepository, TypeORMRepository } from "./repository"
 @generic.template("T", "TID")
 @generic.type("T", "TID")
 class TypeORMControllerGeneric<T, TID> extends RepoBaseControllerGeneric<T, TID>{
-    constructor(){
-        super(x => new TypeORMRepository(x))
+    constructor(fac?: ((x: Class<T>) => Repository<T>)){
+        super(fac ?? (x => new TypeORMRepository(x)))
     }
 }
 
 @generic.template("P", "T", "PID", "TID")
 @generic.type("P", "T", "PID", "TID")
 class TypeORMOneToManyControllerGeneric<P, T, PID, TID> extends RepoBaseOneToManyControllerGeneric<P, T, PID, TID> {
-    constructor(){
-        super((p, t, rel) => new TypeORMOneToManyRepository(p, t, rel))
+    constructor(fac?: ((p: Class<P>, t: Class<T>, rel: string) => OneToManyRepository<P, T>)){
+        super(fac ?? ((p, t, rel) => new TypeORMOneToManyRepository(p, t, rel)))
     }
 }
 
