7eea036991b6a8e99b495d6f77fb1be73db669b3
Ketut Sandiarsa <ktutnik@gmail.com>
2020-09-12 08:48:15 +0800

    Fix failed test

--- packages/plumier/src/index.ts
+++ packages/plumier/src/index.ts
@@ -6,7 +6,7 @@ export {
     domain, Facility, HeaderPart, HttpMethod, HttpStatus, HttpStatusError, Invocation, KoaMiddleware,
     middleware, Middleware, MiddlewareUtil, ParameterBinderMiddleware,
     PlumierApplication, PlumierConfiguration, RequestPart, response, route, RouteInfo,
-    val, validate, ValidatorContext, ValidatorMiddleware, rest, RestDecoratorImpl,
+    val, validate, ValidatorContext, ValidatorMiddleware, 
     RouteDecoratorImpl, CustomBinderFunction, CustomAuthorizerFunction, CustomAuthorizer,
     AuthorizerContext, CustomMiddleware, CustomMiddlewareFunction, FormFile, HttpCookie,
     api
