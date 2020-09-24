18465aedf8813d2268df9c1498027160698424ed
Ketut Sandiarsa <ktutnik@gmail.com>
2020-09-24 10:30:38 +0800

    feat: Bind action result into request hook postSave

--- packages/core/src/request-hook.ts
+++ packages/core/src/request-hook.ts
@@ -1,11 +1,11 @@
 import { reflect } from "tinspector"
 
-import { binder } from "./binder"
+import { BindActionResult, binder } from "./binder"
 import { Class } from "./common"
 import { RequestHookDecorator } from "./decorator/request-hook"
 import { ActionContext, ActionResult, Invocation, Middleware } from "./types"
 
-async function executeHooks(invocation: Invocation<ActionContext>, type: "preSave"|"postSave") {
+async function executeHooks(invocation: Invocation<ActionContext>, type: "preSave"|"postSave", result?:ActionResult) {
     const metadata = invocation.metadata!
     const ctx = invocation.ctx;
     for (const par of metadata.action.parameters) {
@@ -16,7 +16,12 @@ async function executeHooks(invocation: Invocation<ActionContext>, type: "preSav
                 && (x.method.length === 0 || x.method.some(m => m === ctx.method.toLocaleLowerCase()))))
             for (const hook of hooks) {
                 // bind request hook parameters
-                const pars = await binder(hook, ctx)
+                const boundPars = await binder(hook, ctx)
+                // bind action result 
+                const pars = boundPars.map((x, i) => {
+                    const par = hook.parameters[i].decorators.find((x:BindActionResult) => x.kind === "plumier-meta:bind-action-result")
+                    return !!par ? result : x
+                })
                 const value = metadata.actionParams.get(par.name);
                 // execute hook
                 await (par.type.prototype[hook.name] as Function).apply(value, pars)
@@ -29,7 +34,7 @@ export class RequestHookMiddleware implements Middleware<ActionContext> {
     async execute(invocation: Readonly<Invocation<ActionContext>>): Promise<ActionResult> {
         await executeHooks(invocation, "preSave")
         const result = await invocation.proceed()
-        await executeHooks(invocation, "postSave")
+        await executeHooks(invocation, "postSave", result)
         return result
     }
 }
