7dfc6ca381752d444ae89c9a72c92be9ed53a610
Ketut Sandiarsa <ktutnik@gmail.com>
2020-11-30 03:06:46 +0800

    fix: Generic controller get many return data type any

--- packages/core/src/controllers.ts
+++ packages/core/src/controllers.ts
@@ -169,7 +169,7 @@ class RepoBaseControllerGeneric<T = Object, TID = string> implements ControllerG
     @decorateRoute("get", "")
     @api.hideRelations()
     @reflect.type(["T"])
-    async list(offset: number = 0, limit: number = 50, @entity.filter() @reflect.type("T") @val.partial("T") @val.filter() filter: FilterEntity<T>, select: string, order: string, @bind.ctx() ctx: Context): Promise<T[]> {
+    async list(offset: number = 0, limit: number = 50, @entity.filter() @reflect.type("T") @val.partial("T") @val.filter() filter: FilterEntity<T>, select: string, order: string, @bind.ctx() ctx: Context): Promise<any> {
         const transformer = getTransformer(this.constructor as Class, "list")
         const result = await this.repo.find(offset, limit, filter, parseSelect(this.entityType, select), parseOrder(order))
         if (transformer)
@@ -254,7 +254,7 @@ class RepoBaseOneToManyControllerGeneric<P = Object, T = Object, PID = String, T
     @decorateRoute("get", "")
     @api.hideRelations()
     @reflect.type(["T"])
-    async list(@val.required() @reflect.type("PID") pid: PID, offset: number = 0, limit: number = 50, @entity.filter() @reflect.type("T") @val.partial("T") @val.filter() filter: FilterEntity<T>, select: string, order: string, @bind.ctx() ctx: Context): Promise<T[]> {
+    async list(@val.required() @reflect.type("PID") pid: PID, offset: number = 0, limit: number = 50, @entity.filter() @reflect.type("T") @val.partial("T") @val.filter() filter: FilterEntity<T>, select: string, order: string, @bind.ctx() ctx: Context): Promise<any> {
         const transformer = getTransformer(this.constructor as Class, "list")
         const reverseProperty = getGenericControllerReverseRelation(this.constructor as Class)
         await this.findParentByIdOrNotFound(pid)
