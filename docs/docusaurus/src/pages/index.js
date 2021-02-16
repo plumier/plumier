0c4cdc518c9d2fbe6f251d47c5d8c33ffba96064
Ketut Sandiarsa <ktutnik@gmail.com>
2021-02-17 06:08:06 +0800

    docs: Fix feature list on landing page

--- docs/docusaurus/src/pages/index.js
+++ docs/docusaurus/src/pages/index.js
@@ -10,61 +10,32 @@ import styles from './styles.module.css';
 
 const features = [
   {
-      title: 'First Class Entity',
+    title: 'First Class Entity',
 
-      description: (<>
-          <p>
-              Upgrade ORM entity into First Class Entity to have more control to most framework features
-              such as request/response body schema, authorization, validation, generated routes.
-              In the background, Plumier creates CRUD Restful generic controller uses ORM entity as its data model.
+    description: (<>
+      <p>
+        Upgrade ORM entity into First Class Entity to have more control to most framework features
+        such as request/response body schema, authorization, validation, generated routes.
+        In the background, Plumier creates CRUD Restful generic controller uses ORM entity as its data model,
+        example produces:
           </p>
+      <ul>
+        <li><code>POST /items</code> accessible by Supervisor and Staff</li>
+        <li><code>GET /items?offset&amp;limit&amp;filter&amp;select</code> accessible by any login user</li>
+        <li><code>GET /items/&#123;id&#125;</code> accessible by any login user</li>
+        <li><code>POST /items/&#123;id&#125;</code> accessible by Supervisor and Staff</li>
+        <li><code>PUT /items/&#123;id&#125;</code> accessible by Supervisor and Staff</li>
+        <li><code>DELETE /items/&#123;id&#125;</code> accessible by Supervisor and Staff</li>
+      </ul>
 
-          <table>
-              <thead>
-                  <tr><th>Method</th><th>Path</th><th>Accessible By</th></tr>
-              </thead>
-              <tbody>
-                  <tr>
-                      <td>GET</td>
-                      <td><code>/items</code></td>
-                      <td>All login users</td>
-                  </tr>
-                  <tr>
-                      <td>GET</td>
-                      <td><code>/items/&#123;id&#125;</code></td>
-                      <td>All login users</td>
-                  </tr>
-                  <tr>
-                      <td>POST</td>
-                      <td><code>/items</code></td>
-                      <td>Supervisor, Staff</td>
-                  </tr>
-                  <tr>
-                      <td>PUT</td>
-                      <td><code>/items/&#123;id&#125;</code></td>
-                      <td>Supervisor, Staff</td>
-                  </tr>
-                  <tr>
-                      <td>PATCH</td>
-                      <td><code>/items/&#123;id&#125;</code></td>
-                      <td>Supervisor, Staff</td>
-                  </tr>
-                  <tr>
-                      <td>DELETE</td>
-                      <td><code>/items/&#123;id&#125;</code></td>
-                      <td>Supervisor, Staff</td>
-                  </tr>
-              </tbody>
-          </table>
+    </>),
 
-      </>),
+    link: {
+      url: "generic-controller",
+    },
 
-      link: {
-          url: "generic-controller",
-      },
-
-      code:
-          `// create CRUD generic controller on the fly
+    code:
+      `// create CRUD generic controller on the fly
 @route.controller(c => {
   // authorize post, put, patch, delete
   c.mutators().authorize("Supervisor", "Staff");
@@ -93,110 +64,28 @@ export class Item {
   },
 
   {
-      title: 'Policy Based Authorization',
+    title: 'Nested First Class Entity',
 
-      description: (<>
-          <p>
-            Plumier supported securing API with JWT out of the box. 
-            Plumier also provided authorization to restrict user access based on specific logic defined by you.
-            You define your authorization logic in separate location, then apply it using decorators. 
-          </p>
-          <p>
-            In the background Plumier validates your authorization policy to prevent mistyped policy name, 
-            or possibly duplicate/conflict policy name.
-          </p>
-      </>),
+    description: (<>
+      <p>
+        Turn One-To-Many relation of ORM entity into nested API Restful API, that handled by a nested generic controller.
+        </p>
+      <ul>
+        <li><code>POST /posts/&#123;pid&#125;/comments</code></li>
+        <li><code>GET /posts/&#123;pid&#125;/comments?offset&amp;limit&amp;filter&amp;select</code></li>
+        <li><code>GET /posts/&#123;pid&#125;/comments/&#123;id&#125;</code></li>
+        <li><code>POST /posts/&#123;pid&#125;/comments/&#123;id&#125;</code></li>
+        <li><code>PUT /posts/&#123;pid&#125;/comments/&#123;id&#125;</code></li>
+        <li><code>DELETE /posts/&#123;pid&#125;/comments/&#123;id&#125;</code></li>
+      </ul>
+    </>),
 
-      link: {
-          url: "security",
-      },
+    link: {
+      url: "generic-controller",
+    },
 
-      code:
-      `// register with authorization policy builder
-authPolicy()
-  // register role
-  .register("Supervisor", ({ user }) => {
-      // "user" is JWT claim from request header
-      // return true to authorize otherwise false
-      // also possible to return Promise<boolean>
-      return user?.role === "Supervisor"
-  })
-
-  // chain method with next registration
-  .register("Staff", ({ user }) => {
-      return user?.role === "Staff"
-  })`
-  },
-
-  {
-      title: 'Swagger UI with Open API v3',
-
-      description: (<>
-          <p>
-              Open API v3 schema automatically generated from controller metadata. 
-              Mostly no configuration required, but some minor tweak can be applied to get result match your need.
-          </p>
-      </>),
-
-      link: {
-          url: "swagger",
-      },
-
-      image: "/img/swagger.png"
-  },
-
-  {
-      title: 'Nested First Class Entity',
-
-      description: (<>
-          <p>
-              Turn One-To-Many relation of ORM entity into nested API Restful API, that handled by a nested generic controller.
-          </p>
-          <table>
-              <thead>
-                  <tr><th>Method</th><th>Path</th><th>Description</th></tr>
-              </thead>
-              <tbody>
-                  <tr>
-                      <td>GET</td>
-                      <td><code>/posts/&#123;pid&#125;/comments</code></td>
-                      <td>Get post comments with paging, filter etc..</td>
-                  </tr>
-                  <tr>
-                      <td>GET</td>
-                      <td><code>/posts/&#123;pid&#125;/comments/&#123;id&#125;</code></td>
-                      <td>Get post comment by id with projection</td>
-                  </tr>
-                  <tr>
-                      <td>POST</td>
-                      <td><code>/posts/&#123;pid&#125;/comments</code></td>
-                      <td>Create new post comment</td>
-                  </tr>
-                  <tr>
-                      <td>PUT</td>
-                      <td><code>/posts/&#123;pid&#125;/comments/&#123;id&#125;</code></td>
-                      <td>Replace post comment by id</td>
-                  </tr>
-                  <tr>
-                      <td>PATCH</td>
-                      <td><code>/posts/&#123;pid&#125;/comments/&#123;id&#125;</code></td>
-                      <td>Modify post comment by id</td>
-                  </tr>
-                  <tr>
-                      <td>DELETE</td>
-                      <td><code>/posts/&#123;pid&#125;/comments/&#123;id&#125;</code></td>
-                      <td>Delete post comment by id</td>
-                  </tr>
-              </tbody>
-          </table>
-      </>),
-
-      link: {
-          url: "generic-controller",
-      },
-
-      code:
-          `@Entity()
+    code:
+      `@Entity()
 export class Post {
   @PrimaryGeneratedColumn()
   id: number
@@ -218,29 +107,66 @@ export class Comment {
   },
 
   {
-      title: 'Controllers',
+    title: 'Policy Based Authorization',
 
-      description: (<>
-          <p>
-              Require more flexible response result then using First Class Entity? No problem, 
-              you still can use controller to handle user request.
+    description: (<>
+      <p>
+        Plumier supported securing API with JWT out of the box.
+        Plumier also provided authorization to restrict user access based on specific logic defined by you.
+        You define your authorization logic in separate location, then apply it using decorators.
           </p>
-          <p>
-              Plumier controller inspired by ASP.Net MVC controllers, and take important role in Plumier system. 
+      <p>
+        In the background Plumier validates your authorization policy to prevent mistyped policy name,
+        or possibly duplicate/conflict policy name.
           </p>
-          <ul>
-              <li>Routes generated based on controller name and action name, but overridable using decorators</li>
-              <li>Parameter binding using parameter name or decorators</li>
-              <li>Extra data type conversion and simple sanitation for each controller parameter</li>
-              <li>Policy based authorization that applicable using decorators</li>
-          </ul>
-      </>),
+    </>),
 
-      link: {
-          url: "controller",
-      },
+    link: {
+      url: "security",
+    },
 
-      code:
+    code:
+      `// register with authorization policy builder
+authPolicy()
+
+  // register role
+  .register("Supervisor", ({ user }) => {
+      // "user" is JWT claim from request header
+      // return true to authorize otherwise false
+      // also possible to return Promise<boolean>
+      return user?.role === "Supervisor"
+  })
+
+  // chain-able registration method
+  .register("Staff", ({ user }) => {
+      return user?.role === "Staff"
+  })`
+  },
+
+  {
+    title: 'Controllers',
+
+    description: (<>
+      <p>
+        Require more flexible response result then using First Class Entity? No problem,
+        you still can use controller to handle user request.
+          </p>
+      <p>
+        Plumier controller inspired by ASP.Net MVC controllers, and take important role in Plumier system.
+          </p>
+      <ul>
+        <li>Routes generated based on controller name and action name.</li>
+        <li>Parameter binding using parameter name or decorators.</li>
+        <li>Extra data type conversion with simple sanitation for each controller parameter.</li>
+        <li>Policy based authorization that applicable using decorators.</li>
+      </ul>
+    </>),
+
+    link: {
+      url: "controller",
+    },
+
+    code:
       `export class UsersController {
   readonly repo = new TypeORMRepository(User)
 
@@ -254,40 +180,56 @@ export class Comment {
   }
 }`
   },
+
+  {
+    title: 'Swagger UI with Open API v3',
+
+    description: (<>
+      <p>
+        Open API v3 schema automatically generated from controller metadata.
+        Mostly no configuration required, but some minor tweak can be applied to get result match your need.
+        </p>
+    </>),
+
+    link: {
+      url: "swagger",
+    },
+
+    image: "/img/swagger.png"
+  },
 ];
 
 
 function Features() {
-  const { isDarkTheme } = useThemeContext();
-  return (
-    <section className={clsx(styles.features, isDarkTheme ? styles.featuresDark : styles.featuresLight)}>
-      <div className="container">
-        <div className="row">
-          {features.map((props, idx) => (
-            <Feature key={idx} index={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
+  return (<>
+    {features.map((props, idx) => (
+      <Feature key={idx} index={idx} {...props} />
+    ))
+    }
+  </>
   );
 }
 
 function Feature({ description, title, code, link, index, image }) {
+  const isEven = index % 2 === 0
   return (
-    <div className={clsx('col col--12', styles.feature)}>
-      <div className={clsx('row', styles.featureContainer, index % 2 === 0 ? styles.featureContainerOdd : styles.featureContainerEven)}>
-        <div className={clsx('col col--5', styles.codeFont)}>
-          {
-            !!code ? <CodeSnippet code={code} /> : <img className={styles.featureImage} src={image} />
-          }
-        </div>
-        <div className="col col--7">
-          <h1>{title}</h1>
-          <div>{description}</div>
-          <Link to={link.url} className={clsx('button button--primary button--lg', styles.featureButton)}>Learn more about this feature</Link>
+    <section className={clsx(styles.feature, isEven ? styles.sectionDark : styles.sectionLight)}>
+      <div className="container">
+        <div className={clsx('row', styles.featureContainer, isEven ? styles.featureContainerOdd : styles.featureContainerEven)}>
+          <div className={clsx('col col--5', styles.codeFont)}>
+            {
+              !!code ? <CodeSnippet code={code} /> : <img className={styles.featureImage} src={image} />
+            }
+          </div>
+          <div className={clsx("col col--7", styles.featureDescription)}>
+            <h3>Feature</h3>
+            <h1>{title}</h1>
+            <div>{description}</div>
+            <Link to={link.url} className={clsx('button button--secondary button--lg', styles.featureButton)}>Learn more about this feature</Link>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
