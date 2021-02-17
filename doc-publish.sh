e3de46ea41091680a31810f8e8e7b15cdd9b4b35
Ketut Sandiarsa <ktutnik@gmail.com>
2021-02-17 09:53:55 +0800

    docs: Fix feature list on landing page (#795)

--- doc-publish.sh
+++ doc-publish.sh
@@ -6,8 +6,8 @@ git add .
 git commit -m "Update documentation"
 git push
 cd ../..
-git branch -D update-documentation 
-git checkout -b update-documentation
-git add .
-git commit -m "chore: Publish documentation"
-git push -u --no-verify origin update-documentation
+# git branch -D update-documentation 
+# git checkout -b update-documentation
+# git add .
+# git commit -m "chore: Publish documentation"
+# git push -u --no-verify origin update-documentation
