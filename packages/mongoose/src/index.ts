9e092fda583d993281cd0583fa3dbb2ef68af7f4
Ketut Sandiarsa <ktutnik@gmail.com>
2021-03-13 05:03:13 +0800

    Fix failed tests

--- packages/mongoose/src/index.ts
+++ packages/mongoose/src/index.ts
@@ -1,7 +1,7 @@
 import "./validator"
 
 import { model } from "./generator"
-export { model, proxy, MongooseHelper, getDefinition, getModels, models, globalHelper } from "./generator"
+export { model, proxy, MongooseHelper, getDefinition, getModels, models } from "./generator"
 export { collection } from "./decorator"
 export { MongooseFacility } from "./facility"
 export { MongooseControllerGeneric, MongooseOneToManyControllerGeneric } from "./generic-controller"
